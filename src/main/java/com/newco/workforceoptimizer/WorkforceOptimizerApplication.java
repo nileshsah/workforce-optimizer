d2caa956e5abefce1efaae000991bc475370f3b4
Nilesh Sah <nilesh.sah13@gmail.com>
2019-11-03 19:02:35 +0100

    Init spring project with spring initializer

---
+++ src/main/java/com/newco/workforceoptimizer/WorkforceOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.newco.workforceoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceOptimizerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WorkforceOptimizerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/newco/workforceoptimizer/WorkforceOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newco.workforceoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforceOptimizerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
